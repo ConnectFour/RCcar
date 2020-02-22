0cfd1f45f0b8d415185b36745d142466e9163685
ConnectFour <gignac2123@gmail.com>
2020-02-22 18:26:08 -0500

    Update Arduino_Transceiver_ReceiveRadio.ino
    adding motor speed sontrol

--- Arduino/Arduino_Transceiver_ReceiveRadio/Arduino_Transceiver_ReceiveRadio.ino
+++ Arduino/Arduino_Transceiver_ReceiveRadio/Arduino_Transceiver_ReceiveRadio.ino
@@ -3,9 +3,19 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+#define enA 5
+#define in1 6
+#define in2 3
+#define enB 11
+#define in3 9
+#define in4 10
+
 const byte address[6] = "00002"; // Define the transmit pipe
 
 RF24 radio(7, 8); // Create a Radio
+
+int motorSpeedA = 0;
+int motorSpeedB = 0;
 int joystick[2];  // 2 element array holding Joystick readings
 
 float count = 0;
@@ -21,12 +31,21 @@ void setup()   /**** SETUP: RUNS ONCE ****/
   radio.setDataRate(RF24_250KBPS);
   radio.openReadingPipe(0, address);
   radio.startListening();
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
  
 }//--(end setup )-
 
 
 void loop()   /**** LOOP: RUNS CONSTANTLY ****/
+
 {
+  int xAxis = joystick[0];
+
   if ( radio.available() )
   {
     {
@@ -36,6 +55,28 @@ void loop()   /**** LOOP: RUNS CONSTANTLY ****/
       Serial.println(joystick[0]);
       ok ++;
     }
+        if (xAxis < 470)
+        {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    motorSpeedA = map(xAxis, 470, 0, 0, 255);
+    motorSpeedB = map(xAxis, 470, 0, 0, 255);
+        }
+    else if (xAxis > 550)
+        {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    motorSpeedA = map(xAxis, 550, 1023, 0, 255);
+    motorSpeedB = map(xAxis, 550, 1023, 0, 255);
+        }
+  else{
+       motorSpeedA = 0;
+       motorSpeedB = 0;
+      }
   }
   else
   {
@@ -47,12 +88,11 @@ void loop()   /**** LOOP: RUNS CONSTANTLY ****/
   Serial.print("percent of time signal is available ");
   Serial.print(sig);
   Serial.println("%");
+ analogWrite(enA, motorSpeedA);
+ analogWrite(enB, motorSpeedB);
 
-delay(1000);
 }
 
 //--(end main loop )-
 
-
-//NONE
 //*******( THE END )*********
